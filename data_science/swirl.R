60c50beef4a90d0bc8215ac0c244b1f69d088209
Eddy Delta <eddy@intellectionsoftware.com>
2017-03-07 23:34:38 +0000

    Init repo

---
+++ data_science/swirl.R
@@ -0,0 +1,10 @@
+# install.packages("swirl")
+# packageVersion("swirl")
+# library(swirl)
+# install_course("The R Programming Environment")
+# install_from_swirl("R Programming")
+# install_from_swirl("Getting and Cleaning Data")
+# swirl()
+
+#1 8idx2ORky6cKknbFRzrZ
+#2 qDCjkERJz1dYgItqA2Pr
